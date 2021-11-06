4d953234a676be1c362c4457e7fc83210ad662ac
Xavier Belanche <xbelanch@protonmail.com>
2021-11-07 00:40:19 +0100

    Add get filename extension

--- colt.c
+++ colt.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 #include <dirent.h>
+
+const char *get_filename_ext(const char *filename) {
+    const char *dot = strrchr(filename, '.');
+    if(!dot || dot == filename) return NULL;
+    return dot + 1;
+}
 
 int main(int argc, char *argv[])
 {
@@ -10,37 +17,53 @@ int main(int argc, char *argv[])
 
     struct dirent *dp;
     DIR *dir = opendir(".");
+
+    uint8_t __directories = 0;
+    uint8_t __files = 0;
+
+    if (NULL == dir) {
+        fprintf(stdout, "Directory not found. Exit\n");
+        return (1);
+    }
+
+
     while ((dp = readdir(dir)) != NULL) {
         if (dp->d_type == 8) { // is a file?
             if (dp->d_name[0] == '.')
                 continue;
 
-            if (strcmp(dp->d_name, "colt") == 0)
+            if (!get_filename_ext(dp->d_name)) // avoid files without extension
                 continue;
 
-            fprintf(stdout, "%s\n", dp->d_name);
+            // Print only markdown files
+            if (strcmp(get_filename_ext(dp->d_name), "md") == 0) {
+                // Read and output the content of file
+                FILE *fp = fopen(dp->d_name, "rb");
+                char chr;
+                while ((chr = getc(fp)) != EOF) {
+                    fprintf(stdout, "%c", chr);
+                }
 
-            // Read and output the content of file
-            FILE *fp = fopen(dp->d_name, "rb");
-            char chr;
-            while ((chr = getc(fp)) != EOF) {
-                fprintf(stdout, "%c", chr);
+                fclose(fp);
             }
 
-            fclose(fp);
+            __files++;
         }
 
-        if (dp->d_type == 4) { // is a directory
+        if (dp->d_type == 4) { // found a directory
 
             if (strcmp(".", dp->d_name) == 0
                 || strcmp("..", dp->d_name) == 0
                 || dp->d_name[0] == '.')
                 continue;
 
+            __directories++;
+
             fprintf(stdout, "%s\n", dp->d_name);
         }
     }
 
+    fprintf(stdout, "Found %d directories and %d files ", __directories, __files);
     closedir(dir);
-    return 0;
+    return (0);
 }
