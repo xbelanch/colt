070a3d987c5dbbbead21082243958a98c2e5e3ca
Xavier Belanche <xbelanch@protonmail.com>
2021-11-09 23:25:34 +0100

    Add naive match TODO pattern

--- colt.c
+++ colt.c
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include <dirent.h>
 
+#define MAX_LINE_SIZE 1024
+
 const char *get_filename_ext(const char *filename) {
     const char *dot = strrchr(filename, '.');
     if(!dot || dot == filename) return NULL;
@@ -36,14 +38,32 @@ int main(int argc, char *argv[])
                 continue;
 
             // Print only markdown files
-            if (strcmp(get_filename_ext(dp->d_name), "md") == 0) {
+            if (strcmp(get_filename_ext(dp->d_name), "c") == 0) {
                 // Read and output the content of file
                 FILE *fp = fopen(dp->d_name, "rb");
-                char chr;
-                while ((chr = getc(fp)) != EOF) {
-                    fprintf(stdout, "%c", chr);
-                }
+                fprintf(stdout, "Parse file: %s\n", dp->d_name);
+                char *line = malloc(sizeof(char) * MAX_LINE_SIZE);
+                while (fgets(line, MAX_LINE_SIZE - 1, fp)) {
+                    // Discard blank lines
+                    if (line[0] == '\n')
+                        continue;
+                    // Remove trailing  newline
+                    line[strcspn(line, "\n")] = '\0';
 
+                    // Remove spaces or tabs
+                    char *d = line; do { d++; } while (*d == ' ');
+
+                    // Print if match some pattern
+                    // TODO: It works?
+                    char *r = malloc(sizeof(char) * MAX_LINE_SIZE);
+                    fprintf(stdout, "%ld - %ld\n", strlen(line), strlen(line));
+                    if (strstr(line, "// TODO: ") != NULL) {
+                        memcpy(r, d, MAX_LINE_SIZE);
+                        fprintf(stdout, "%s\n", r);
+                    }
+
+                }
+                free(line);
                 fclose(fp);
             }
 
